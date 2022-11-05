67994995972abf754c6066bfb545808f06266e55
Liza N <el507577@gmail.com>
2022-11-05 11:10:44 +0600

    начала оформлять страницу
    Вывела на страницу компоненты. Добавила css для гамбургера и вывела картинки. Создала компонент Ingredients и создала в нем див.

--- src/App.tsx
+++ src/App.tsx
@@ -12,25 +12,56 @@ import Burger from "./Components/Cards/Burger";
 
 const INGREDIENTS: IngredientType[] = [
   {name: 'Meat', price: 80, image: meat},
-  {name: 'Chess', price: 50, image: chess},
+  {name: 'Cheese', price: 50, image: chess},
   {name: 'Salat', price: 10, image: salat},
   {name: 'Bekon', price: 60, image: bekon},
 ];
 
 function App() {
+  const [ingredients, setIngredients] = useState([
+    {name: 'Meat', count: 0},
+    {name: 'Cheese', count: 0},
+    {name: 'Salat', count: 0},
+    {name: 'Bekon', count: 0},
+  ]);
+
+  const addCount = (index: number) => {
+    const ingredientsCopy = ingredients.map(ingredients => {
+      return {
+        ...ingredients,
+        count: ingredients.count + 1,
+      }
+    });
+  }
+
+
+  const deletePerson = (index: number) => {
+    const ingredientsCopy = [...ingredients];
+    ingredientsCopy.splice(index, 1);
+    setIngredients(ingredientsCopy);
+  }
+
+
   return (
     <div className="container">
       <div className="wrapper">
         <h3>Ingredients</h3>
         <div className="ing-wrap">
           {INGREDIENTS.map((item, index) => (
-          <Ingredients key={index} item={item}/>
-        ))}</div>
+            <Ingredients
+              key={index}
+              item={item}
+              count={ingredients[index].count}
+              onHandlerClick={() => addCount(index)}
+              delete={() => deletePerson(index)}
+            />
+          ))}</div>
 
       </div>
       <div className="wrapper">
         <h3>Burger</h3>
-        <Burger />
+        <Burger/>
+        <p className="text-center fs-4 fw-bold">Price: </p>
       </div>
     </div>
   );
--- src/Components/Ingredients/Ingredients.tsx
+++ src/Components/Ingredients/Ingredients.tsx
@@ -3,18 +3,22 @@ import IngredientType from "../../types d";
 
 interface PropsIngredients {
   item: IngredientType;
+  count: number;
+  onHandlerClick: React.MouseEventHandler;
+  delete: React.MouseEventHandler;
 }
+
 
 const Ingredients: React.FC<PropsIngredients> = (props) => {
   return (
-    <div className="d-flex justify-content-between mt-4">
-      <span>
-          <img src={props.item.image} alt="Ingredient image"/>
-          <p className="d-inline-block">{props.item.name}</p>
+    <div className="d-flex justify-content-between m-4">
+      <span onClick={props.onHandlerClick}>
+          <img  src={props.item.image} alt="Ingredient image"/>
       </span>
+      <p className="d-inline-block ms-3">{props.item.name}</p>
       <span className="m-4">
-        <b className="m-5">x 1</b>
-        <button className="m-2">Delete</button>
+        <b className="m-5">x {props.count}</b>
+        <button onClick={props.delete} className="m-2">Delete</button>
       </span>
 
     </div>
